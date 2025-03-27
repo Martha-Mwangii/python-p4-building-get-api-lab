70810ec687f867647a3431ce0ad944513a68f44f
Martha Wangui Mwangi <marthawm7252@gmail.com>
2025-03-27 13:21:42 +0300

    server

--- server/models.py
+++ server/models.py
@@ -11,30 +11,18 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
-    serialize_rules = ('-baked_goods.bakery',)
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    baked_goods = db.relationship('BakedGood', backref='bakery')
-
-    def __repr__(self):
-        return f'<Bakery {self.name}>'
+    baked_goods = db.relationship("BakedGood", backref="bakery", lazy=True)
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
-    serialize_rules = ('-bakery.baked_goods',)
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
-
-    def __repr__(self):
-        return f'<Baked Good {self.name}, ${self.price}>'
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"), nullable=False)
